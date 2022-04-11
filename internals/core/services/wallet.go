93e42ad265b987db66a0d9e6c937bbc04fef61ab
udodinho <udodinho200@gmail.com>
2022-04-11 11:43:48 +0100

    Change the return value for getBalance from *wallet.User to *wallet.Wallet

--- internals/core/services/wallet.go
+++ internals/core/services/wallet.go
@@ -1,8 +1,8 @@
 package services
 
 import (
+	"github.com/udodinho/golangProjects/wallet-engine/internals/core/domain/wallet"
 	"github.com/udodinho/golangProjects/wallet-engine/internals/ports"
-	"github.com/udodinho/goprojects/wallet-engine/domain/wallet"
 )
 
 type WalletService struct {
@@ -21,19 +21,19 @@ func (w *WalletService) CheckPassword(userRef string) ([]*wallet.User, error) {
 	return w.WalletRepo.CheckPassword(userRef)
 }
 
-func (w *WalletService) GetAccountBalance(userID string) ([]*wallet.User, error) {
-	return w.WalletRepo.GetAccountBalance(userID)
+func (w *WalletService) GetBalance(userID string) ([]*wallet.Wallet, error) {
+	return w.WalletRepo.GetBalance(userID)
 }
 
 func (w *WalletService) ChangeStatus(isActive bool, userRef string) (interface{}, error) {
 	return w.WalletRepo.ChangeStatus(isActive, userRef)
 }
 
-func (w *WalletService) PostTransaction(transaction *wallet.Wallet) (*wallet.Wallet, error) {
+func (w *WalletService) PostTransaction(transaction *wallet.Wallet) (interface{}, error) {
 	return w.WalletRepo.PostTransaction(transaction)
 }
 
-func (w *WalletService) SaveTransaction(transaction *wallet.Wallet) (*wallet.Wallet, error) {
+func (w *WalletService) SaveTransaction(transaction *wallet.Transaction) (interface{}, error) {
 	return w.WalletRepo.SaveTransaction(transaction)
 }
 
