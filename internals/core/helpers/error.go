570f5eb686b13ca91a375eb3461a14b4fb5dddfe
udodinho <udodinho200@gmail.com>
2022-04-11 11:38:42 +0100

    Add string method for error handling

--- internals/core/helpers/error.go
+++ internals/core/helpers/error.go
@@ -4,29 +4,39 @@ import (
 	"fmt"
 	"github.com/go-playground/validator"
 	"github.com/google/uuid"
+	"strings"
 	"time"
 )
+
+type FieldError struct {
+	err validator.FieldError
+}
+
+func (f FieldError) String() string {
+	var sb strings.Builder
+
+	sb.WriteString("validation failed on field '" + f.err.Field() + "'")
+	sb.WriteString(", condition: " + f.err.ActualTag())
+
+	// Print condition parameters, e.g. one_of=red blue -> { red blue }
+	if f.err.Param() != "" {
+		sb.WriteString(" { " + f.err.Param() + " }")
+	}
+
+	if f.err.Value() != nil && f.err.Value() != "" {
+		sb.WriteString(fmt.Sprintf(", actual: %v", f.err.Value()))
+	}
+
+	return sb.String()
+}
+
+func NewFieldError(err validator.FieldError) FieldError {
+	return FieldError{err: err}
+}
 
 func (err ErrorResponse) Error() string {
 	var errorBody ErrorBody
 	return fmt.Sprintf("%v", errorBody)
-}
-
-func ErrorArrayToError(errorBody []validator.FieldError) error {
-	var errorResponse ErrorResponse
-	errorResponse.TimeStamp = time.Now().Format(time.RFC3339)
-	errorResponse.ErrorReference = uuid.New()
-
-	for _, value := range errorBody {
-		body := ErrorBody{
-			Code:    "400 {validation error}",
-			Message: value,
-			Source:  Config.AppName,
-		}
-		errorResponse.Errors = append(errorResponse.Errors, body)
-
-	}
-	return errorResponse
 }
 
 func PrintErrorMessage(code, message string) error {
