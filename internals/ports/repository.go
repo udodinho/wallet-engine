bc18cf34d777e5ec26f57fa435da92a5befd5e7f
udodinho <udodinho200@gmail.com>
2022-04-11 11:48:05 +0100

    Change PostTransaction, SaveTransaction return value to an interface

--- internals/ports/repository.go
+++ internals/ports/repository.go
@@ -1,13 +1,13 @@
 package ports
 
-import "github.com/udodinho/goprojects/wallet-engine/domain/wallet"
+import "github.com/udodinho/golangProjects/wallet-engine/internals/core/domain/wallet"
 
 type WalletRepository interface {
 	CreateWallet(wallet *wallet.User) (*wallet.User, error)
 	GetUserByEmail(email string) ([]*wallet.User, error)
 	CheckPassword(userRef string) ([]*wallet.User, error)
-	GetAccountBalance(userID string) ([]*wallet.User, error)
+	GetBalance(userID string) ([]*wallet.Wallet, error)
 	ChangeStatus(isActive bool, userRef string) (interface{}, error)
-	PostTransaction(transaction *wallet.Wallet) (*wallet.Wallet, error)
-	SaveTransaction(transaction *wallet.Wallet) (*wallet.Wallet, error)
+	PostTransaction(transaction *wallet.Wallet) (interface{}, error)
+	SaveTransaction(transaction *wallet.Transaction) (interface{}, error)
 }
