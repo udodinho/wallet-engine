ff707f7b5d388e7f3d2d6d4340e6751974546dfe
udodinho <udodinho200@gmail.com>
2022-04-11 11:36:38 +0100

    Add NewFieldError

--- internals/core/helpers/decode.go
+++ internals/core/helpers/decode.go
@@ -3,7 +3,6 @@ package helpers
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator"
-	"github.com/udodinho/goprojects/wallet-engine/infrastructure/servererrors"
 )
 
 func Decode(ctx *gin.Context, obj interface{}) []string {
@@ -13,7 +12,7 @@ func Decode(ctx *gin.Context, obj interface{}) []string {
 		errVal, ok := err.(validator.ValidationErrors)
 		if ok {
 			for _, fieldErr := range errVal {
-				errs = append(errs, servererrors.NewFieldError(fieldErr).String())
+				errs = append(errs, NewFieldError(fieldErr).String())
 			}
 		} else {
 			errs = append(errs, "Internal server error: "+err.Error())
