ea25a188edc15d25ca7c195ec57cdc3ef9fc1d90
udodinho <udodinho200@gmail.com>
2022-04-11 11:13:57 +0100

    Add json tag to transaction and wallet struct

--- internals/core/domain/wallet/wallet.go
+++ internals/core/domain/wallet/wallet.go
@@ -1,19 +1,18 @@
-package domain
+package wallet
 
 // Transaction struct
 type Transaction struct {
-	UserID         string
-	TransactionRef string
-	PhoneNumber    string
-	Amount         float64
-	Password       string
+	UserID         string  `json:"user_id"bson:"user_id"`
+	TransactionRef string  `json:"transaction_ref"bson:"transaction_ref"`
+	PhoneNumber    string  `json:"phone_number"bson:"phone_number"`
+	Amount         float64 `json:"amount"bson:"amount"`
+	Password       string  `json:"-"bson:"password"`
 }
 
 // Wallet struct
 type Wallet struct {
-	UserID    string
-	Balance   float64
-	Reference string
+	UserID  string  `json:"user_id"bson:"user_id"`
+	Balance float64 `json:"balance"bson:"balance"`
 }
 
 // CreditWallet credits the user wallet
@@ -26,4 +25,8 @@ func (w *Wallet) CreditWallet(money float64, userID string) {
 func (w *Wallet) DebitWallet(money float64, userID string) {
 	w.UserID = userID
 	w.Balance = w.Balance - money
+}
+
+func (u *User) ActivateWallet(active bool) {
+	u.IsActive = active
 }
